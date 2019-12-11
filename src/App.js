c85a1236e860046a83cbbdd2d59bbd841d802cbc
adilShehezad <adildad2@gmail.com>
2019-12-11 15:06:39 -0800

    initial massage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// navbars
+
+import Menu from './components/Menu';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.js
@@ -0,0 +1,18 @@
+// components{Home, Shop , singleProduct, Contact , Checkout, Cart, Category with dropdown}
+import React, { Component,Fragment } from 'react'
+// import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+/*import TopNav from './TopNav';
+import SecondNav from './SecondNav';
+import MainNav from './MainNav';
+*/
+class Menu extends Component {
+    render() {
+        return (
+            <Fragment>
+                hello
+            </Fragment>
+        )
+    }
+}
+
+export default Menu
